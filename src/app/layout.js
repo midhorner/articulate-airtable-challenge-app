5c30670976899bd61932bd63840017f18221b11a
Drew Horner <horner.andrew.e@gmail.com>
2025-04-22 19:49:46 -0400

    integrate chakra ui, updated config.mjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,16 @@
+import { Provider } from "@/components/ui/provider";
+import { Theme } from "@chakra-ui/react";
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
+    <html lang="en" suppressHydrationWarning style={{colorScheme: "light",backgroundColor: "white"}}>
+        <body>
+          <Provider>
+            <Theme appearance="light">
+              {children}
+            </Theme>            
+          </Provider>
+        </body>
     </html>
   );
 }
--- src/components/RecordsTable.js
+++ src/components/RecordsTable.js
@@ -1,10 +1,11 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const RecordsTable = (dataRecords) => {
   return (
-    <>
-    {dataRecords.records.map((record) => (<div key={record.id}>{record.bugID}</div>))}
-    </>
+    <Box>
+    {dataRecords.records.map((record) => (<div key={record.recordID}>{record.bugID}{console.log(record)}</div>))}
+    </Box>
   )
 }
 

5afd03560f9f4ca87f12aef667a73a739ecc5da4
Drew Horner <horner.andrew.e@gmail.com>
2025-04-22 23:03:08 -0400

    added airtable api call, some code touch ups

--- src/components/DropdownInput.js
+++ src/components/DropdownInput.js
@@ -1,5 +1,8 @@
 import { Select, Portal, createListCollection } from "@chakra-ui/react"
 import { useState } from "react";
+// Airtable API key
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'pat9DSbh293oCwas2.d1752f597897fb0c291e1351252b15d4ad253777ade95ffe0536674c334ccbb7'}).base('appA2Q8AsRUbmN7jE');
 
 // use chakra createListCollection for select boxes per documentation
 const statuses = createListCollection({
@@ -19,19 +22,31 @@ const priorities = createListCollection({
     ],
 });
 
-const DropdownInput = ({ getValue, column }) => {
+// cell context props
+const DropdownInput = ({ getValue, row, column }) => {
     const initialValue = getValue();
     const [value, setValue] = useState(initialValue);
     const dropdownItems = column.columnDef.header == 'Priority' ? priorities : statuses;
-
+    
+    // API call on select
+    const updateAirtable = (value) => {
+        setValue(value);
+        const jsonRecord = {
+            "id": `${row.original.recordID}`,
+            "fields": {}
+        }
+        jsonRecord.fields[column.columnDef.header] = value;
+        base('Bugs').update([jsonRecord]);
+    };
+    // documentation was pretty straightforward
     return (
         <Select.Root collection={dropdownItems} size="sm">
         <Select.HiddenSelect />
         <Select.Control>
             <Select.Trigger>
-            <Select.ValueText placeholder={value} />
+                <Select.ValueText placeholder={value} />
             </Select.Trigger>
-            <Select.IndicatorGroup>
+                <Select.IndicatorGroup>
             <Select.Indicator />
             </Select.IndicatorGroup>
         </Select.Control>
@@ -39,7 +54,7 @@ const DropdownInput = ({ getValue, column }) => {
             <Select.Positioner>
             <Select.Content>
                 {dropdownItems.items.map((item) => (
-                <Select.Item item={item} key={item.value}>
+                <Select.Item item={item} key={item.value} onClick={() => {updateAirtable(item.value)}}>
                     {item.label}
                     <Select.ItemIndicator />
                 </Select.Item>
